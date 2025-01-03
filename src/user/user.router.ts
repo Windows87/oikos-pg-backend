a58ec16711886588cb371451ccabe43a3c95b0c5
Yuri Faria <yuri.yurifaria@gmail.com>
2025-01-03 20:49:02 -0300

    feat: change patch to post user group

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -7,6 +7,6 @@ const router = express.Router()
 router.route('/login').post(controller.login)
 router.route('/register').post(controller.register)
 router.route('/me').all(auth).get(controller.getMe)
-router.route('/me/group').all(auth).patch(controller.setUserGroup)
+router.route('/me/group').all(auth).post(controller.setUserGroup)
 
 export default router
